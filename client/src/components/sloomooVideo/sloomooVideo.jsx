acc526884b5b425447f76caf40329542ebc696bf
Maddie Hong <maddiehong@Maddies-MacBook-Pro.local>
2024-12-09 10:52:22 -0800

    credit fix

--- client/src/components/sloomooVideo/sloomooVideo.jsx
+++ client/src/components/sloomooVideo/sloomooVideo.jsx
@@ -34,7 +34,6 @@ Sweet Sloomoo sits, all alone.`}, {videoSrc:'/videos/Sloomoovie_part3.mp4', poem
                                     width: '100%',
                                     height: '100%',
                                     position: 'relative',
-                                    objectFit: 'cover'
                                 }} />
                 </div>
                 <div className="sloomoo-holiday-wish-header">
